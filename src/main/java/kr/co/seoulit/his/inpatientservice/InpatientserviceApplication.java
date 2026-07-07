311f0e64046450e6ccf16db1e6a1de5b75260283
youngeiya <kim.ga.young467@gmail.com>
2026-07-07 11:13:58 +0900

    first commit

---
+++ src/main/java/kr/co/seoulit/his/inpatientservice/InpatientserviceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.inpatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpatientserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InpatientserviceApplication.class, args);
+    }
+
+}
